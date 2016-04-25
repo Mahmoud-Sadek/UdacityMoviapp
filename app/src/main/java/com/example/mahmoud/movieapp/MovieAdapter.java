b21be70537a48ae9ee4a13e6418cc3edc758bbc0
Mahmoud <mahmoud.sadek.96@gmail.com>
2016-04-25 17:23:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mahmoud/movieapp/MovieAdapter.java
@@ -0,0 +1,52 @@
+package com.example.mahmoud.movieapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mahmoud on 4/25/2016.
+ */
+public class MovieAdapter extends BaseAdapter {
+
+    Context context;
+    MovieData [] movieData;
+    public MovieAdapter(Context context, MovieData [] movieData) {
+        this.context = context;
+        this.movieData = movieData;
+    }
+
+    @Override
+    public int getCount() {
+        return movieData.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return movieData[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null){
+            view = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_item_movie,viewGroup,false);
+        }
+        ImageView imgView = (ImageView) view.findViewById(R.id.list_item_movie_imageView);
+        String baseUrl = "http://image.tmdb.org/t/p/w185";
+        String poster_url = baseUrl+movieData[i].getPoster_url();
+        Picasso.with(context).load(poster_url).into(imgView);
+        return view;
+    }
+}
